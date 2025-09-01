e7e01e5901c447aa50c0d22bdde7efce6e6bc130
Mahmud2640 <mdmahmud484@gmail.com>
2025-09-02 00:08:38 +0600

    type

---
+++ Milestone 4 (JavaScript)/type.js
@@ -0,0 +1,14 @@
+// Sting type data
+var name = " Abdullah Al Mahmud";
+var address = "Dhaka, Bangladesh";
+
+// Number type data
+var age = 22;
+var number = 1792492640;
+
+// Boolean type data
+var male = true;
+var female = false;
+
+// typeof is check the type of data
+console.log(typeof age);
